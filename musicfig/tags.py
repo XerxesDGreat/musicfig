67a43ca0d55f3c238bafdf9377216da9d887ae37
Josh Wickham <josh@i-josh.com>
2021-12-07 14:21:53 -0800

    add slack tag, update verification, add app ctxt

--- musicfig/tags.py
+++ musicfig/tags.py
@@ -12,11 +12,12 @@ logger = logging.getLogger(__name__)
 
 
 class NFCTag():
-    def __init__(self, identifier, required_kwargs=[], **kwargs):
+    def __init__(self, identifier, required_kwargs=[], app_context=None, **kwargs):
         self.identifier = identifier
+        self.app_context = app_context
         self._verify_kwargs(required_kwargs, **kwargs)
     
-    def _verify_kwargs(required_kwargs, **kwargs):
+    def _verify_kwargs(self, required_kwargs, **kwargs):
         for required_kwarg in required_kwargs:
             if required_kwarg not in kwargs:
                 raise KeyError("missing required key '%s'" % required_kwarg)
@@ -45,17 +46,7 @@ class UnknownTag(NFCTag):
         return colors.RED
 
 
-class WebhookTag(NFCTag):
-    required_kwargs = ["url"]
-
-    def __init__(self, identifier, **kwargs):
-        super().__init__(identifier, WebhookTag.required_kwargs, **kwargs)
-        self.webhook_url = kwargs["url"]
-        
-    def on_add(self):
-        super().on_add()
-        self._post_to_url(self.webhook_url)
-
+class WebhookMixin():
     def _post_to_url(self, url, request_body={}):
         try:
             return webhook.Requests.post(url, request_body)
@@ -63,9 +54,47 @@ class WebhookTag(NFCTag):
             logger.exception("Failed to execute webhook")
 
 
+class WebhookTag(NFCTag, WebhookMixin):
+    required_kwargs = ["url"]
+
+    def __init__(self, identifier, app_context=None, **kwargs):
+        super().__init__(identifier,
+            required_kwargs=WebhookTag.required_kwargs,
+            app_context=app_context,
+            **kwargs)
+        self.webhook_url = kwargs["url"]
+        
+    def on_add(self):
+        super().on_add()
+        self._post_to_url(self.webhook_url)
+
+
+class SlackTag(NFCTag, WebhookMixin):
+    required_kwargs = ["text"]
+
+    def __init__(self, identifier, app_context=None, **kwargs):
+        super().__init__(
+            identifier,
+            required_kwargs=SlackTag.required_kwargs,
+            app_context=app_context,
+            **kwargs
+        )
+        self.webhook_url = app_context.config.get("SLACK_WEBHOOK_URL")
+        self.text = kwargs["text"]
+    
+    def on_add(self):
+        super().on_add()
+        self._post_to_url(self.webhook_url, {"text": self.text})
+
+
+
+
+
+
 class Tags():
 
-    def __init__(self, should_load_tags=True):
+    def __init__(self, app_context=None, should_load_tags=True):
+        self.app_context = app_context
         current_dir = os.path.dirname(os.path.abspath(__file__))
         if Path(current_dir + '/../tags.yml').is_file():
             self.tags_file = current_dir + '/../tags.yml'
@@ -86,7 +115,7 @@ class Tags():
         if (self.last_updated != os.stat(self.tags_file).st_mtime):
             with open(self.tags_file, 'r') as stream:
                 self.tags = yaml.load(stream, Loader=yaml.FullLoader)
-            self._tags = {k: Tags.tag_factory(k, v) for (k,v) in self.tags.items()}
+            self._tags = {k: self.tag_factory(k, v) for (k,v) in self.tags.items()}
             self._tags = {k: v for (k, v) in self._tags.items() if v is not None}
             self.last_updated = os.stat(self.tags_file).st_mtime
             logger.info("loaded %s into new form of tags, %s into old form of", len(self._tags), len(self.tags))
@@ -98,7 +127,7 @@ class Tags():
         "webhook": WebhookTag,
 
     }
-    def tag_factory(identifier, tag_definition):
+    def tag_factory(self, identifier, tag_definition):
         # TODO build a composite tag in case we want to do e.g. spotify + webhook;
         # perhaps do a list of types or something?
         tag = None
@@ -110,7 +139,7 @@ class Tags():
         if tag_class is None:
             return tag
 
-        return tag_class(identifier, **tag_definition)
+        return tag_class(identifier, app_context=self.app_context, **tag_definition)
 
 
     def get_tag_by_identifier(self, identifier):
