e22c96509382ce13ef82d93fd8972c91cb845241
Josh Wickham <josh@i-josh.com>
2021-12-09 23:54:41 -0800

    visibility

--- musicfig/events.py
+++ musicfig/events.py
@@ -1,7 +1,12 @@
+import logging
+
 from flask_socketio import SocketIO
 from . import socketio
 from flask_socketio import send
 
+logger = logging.getLogger(__name__)
+
 @socketio.on('message')
 def on_message(message):
+    logger.info("fasdfasdfasdfasdfasdfasdf")
     send(message)
