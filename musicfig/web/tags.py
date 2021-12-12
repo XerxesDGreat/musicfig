6cd0fe35af94a99723af6ad8ec67adb984be284e
Josh Wickham <josh@i-josh.com>
2021-12-11 20:17:23 -0800

    new page for creating a tag

--- musicfig/web/tags.py
+++ musicfig/web/tags.py
@@ -15,3 +15,8 @@ logger = logging.getLogger(__name__)
 def tag_list():
     logger.info("listing tags")
     return render_template("tags.html", nfc_tags=NFCTagStore.get_all_nfc_tags())
+
+@web.route("/tags/create", methods=["GET"])
+def tag_create():
+    new_tag_id = request.args.get("tag_id")
+    return render_template("create_tag.html", tag_id=new_tag_id)
