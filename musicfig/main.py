37d108fd50d6afedbfe4bd9db50b691fb65e03fd
Josh Wickham <josh@i-josh.com>
2021-12-22 09:03:41 -0800

    oops, define the variable we're using

--- musicfig/main.py
+++ musicfig/main.py
@@ -18,6 +18,8 @@ class MainLoop(threading.Thread):
     event dispatcher for later handling.
     """
 
+    USB_ERROR_THRESHOLD = 5
+
     def __init__(self, group=None, target=None, name=None, args=(), kwargs=None, *, daemon=None):
         super().__init__(group=group, target=target, name=name, args=args, kwargs=kwargs, daemon=daemon)
         self.current_active_tags = set() # this may be better in the Dimensions
